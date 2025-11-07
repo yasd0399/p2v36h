faadd70b9eb914deba5c695037364bfccb472b87
yasd0399 <yasd0399@gmail.com>
2025-11-07 21:34:44 +0500

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -1,4 +1,9 @@
 // Logs text to console
-function log(n, ...t) {
-  console.log(`[${n.toUpperCase()}]`, ...t);
+function log(n, ...m) {
+  console.log(`[${n.toUpperCase()}]`, ...m);
+}
+
+// Gets url path
+function getURLPath(u) {
+  return !u.startsWith('https') ? u.split('http://')[1].split('/') : u.startsWith('https') ? u.split('https://')[1].split('/') : null;
 }
