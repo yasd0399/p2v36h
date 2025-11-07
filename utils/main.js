67939812113336a3af28a8105db131652c399f10
yasd0399 <yasd0399@gmail.com>
2025-11-07 22:55:35 +0500

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -1,9 +1,9 @@
-// Logs text to console
-var log = function(n, ...m) {
-  console.log(`[${n.toUpperCase()}]`, ...m);
-}
-
 // Gets url path
 var getURLPath = function(u) {
   return u.split('/').slice(2);
+}
+
+// Logs text to console
+var log = function(n, ...m) {
+  console.log(`[${n.toUpperCase()}]`, ...m);
 }
