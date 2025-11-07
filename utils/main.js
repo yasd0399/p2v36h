63e3092236103da3585a62f129d60227c8c49281
yasd0399 <yasd0399@gmail.com>
2025-11-07 23:12:50 +0500

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -1,10 +1,9 @@
 // Gets url path
-var getURLPath = function(u) {
-  if (!u.startsWith('http')) return null;
-  return u.split('/').slice(2);
+var getURLPath=function(u){
+  return u.startsWith('http')&&u.split('/').slice(2);
 }
 
 // Logs text to console
-var log = function(n, ...m) {
-  console.log(`[${n.toUpperCase()}]`, ...m);
+var log=function(n, ...m){
+  console.log(`[${n.toUpperCase()}]`,...m);
 }
