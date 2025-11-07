6e6954084f382362294c95f9fd2e85688de517da
yasd0399 <yasd0399@gmail.com>
2025-11-07 22:32:11 +0500

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -3,12 +3,7 @@ function log(n, ...m) {
   console.log(`[${n.toUpperCase()}]`, ...m);
 }
 
-console.log('Utils loaded: log function defined');
-
 // Gets url path
 function getURLPath(u) {
-  console.log('getURLPath called with:', u); // ← добавим логирование
   return !u.startsWith('https') ? u.split('http://')[1].split('/') : u.startsWith('https') ? u.split('https://')[1].split('/') : null;
 }
-
-console.log('Utils loaded: getURLPath function defined');
